95a9440f6977a183732b44160fbe744288de5b13
hatsunemiku <hatsunemiku@126.com>
2024-11-02 16:06:36 +0800

    edit config mk file

--- makefiles/project/fankeh7_eval/general_config.mk
+++ makefiles/project/fankeh7_eval/general_config.mk
@@ -6,25 +6,6 @@
 PROJ_NAME         :=  fankeh7_eval
 
 #
-# Type of project
-#
-PROJ_MOD_APP      :=  binimg
-
-#
-# Optional settings.
-# Some settings are defined by default in specific makefiles. If you want to
-# apply specific settings, use this section to uncomment and define them.
-# Check documentation for explanation of each setting and allowed values.
-#
-PROJ_OPT_FEATURES     :=  
-PROJ_FAST_ENABLE      :=  
-
-#
-# Operating system used in this project
-#
-PROJ_MOD_OS       :=  FreeRTOS
-
-#
 # Toolchain used in this project
 #
 PROJ_TC           :=  armclang
