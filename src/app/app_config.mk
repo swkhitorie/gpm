af17c9a8d5fda365471e4ee164bfbf48828f0002
hitorie_swk <hitorie_swk@126.com>
2025-01-15 19:01:42 +0800

    [make] modify makelist

--- src/app/app_config.mk
+++ src/app/app_config.mk
@@ -38,15 +38,66 @@ PROJ_OPENOCD_LOAD_ADDR := 0x08000000
 MOD_ARCH  =  m7
 
 #####################################
-# module configuration
+# platform os configuration
 #####################################
+FR_ROOTDIR := ${SDK_ROOTDIR}/platforms/frtos
+CONFIG_FR_ARCH=m7
+CONFIG_FR_TOOLCHAIN=gcc
+CONFIG_FR_MEM_METHOD=4
+CONFIG_FR_POSIX=y
+CONFIG_FR_USB=y
+CONFIG_FR_PX4_SUPPORT=y
+include ${FR_ROOTDIR}/Make.mk
 
-include ${SDK_ROOTDIR}/src/app/module_config.mk
+CSOURCES          += ${addprefix platforms/frtos/,${FR_CSOURCES}}
+
+CPPSOURCES        += ${addprefix platforms/frtos/,${FR_CPPSOURCES}}
+
+PROJ_CINCDIRS     += ${addprefix platforms/frtos/,${FR_CINCDIRS}}
+
+#####################################
+# px4 Layer configuration
+#####################################
+PROJ_CDEFS += __PX4_FR
+
+PROJ_CINCDIRS += platforms/common/include
+PROJ_CINCDIRS += platforms/common/include/px4_platform_common
+PROJ_CINCDIRS += platforms/common/include/px4_platform_common/px4_work_queue
+PROJ_CINCDIRS += src/lib/cdev
+PROJ_CINCDIRS += src/lib/cdev/posix
+PROJ_CINCDIRS += src/lib/perf
+PROJ_CINCDIRS += src/include
+PROJ_CINCDIRS += src/lib/systemlib
+PROJ_CINCDIRS += src/drivers
+PROJ_CINCDIRS += src
+PROJ_CINCDIRS += src/lib
+PROJ_CINCDIRS += src/modules
+
+CPPSOURCES += platforms/common/px4_work_queue/ScheduledWorkItem.cpp
+CPPSOURCES += platforms/common/px4_work_queue/WorkItem.cpp
+CPPSOURCES += platforms/common/px4_work_queue/WorkItemSingleShot.cpp
+CPPSOURCES += platforms/common/px4_work_queue/WorkQueue.cpp
+CPPSOURCES += platforms/common/px4_work_queue/WorkQueueManager.cpp
+CPPSOURCES += platforms/common/module.cpp
+CPPSOURCES += platforms/common/px4_log.cpp
+
+CPPSOURCES += src/lib/cdev/posix/cdev_platform.cpp
+CPPSOURCES += src/lib/cdev/CDev.cpp
+CPPSOURCES += src/lib/perf/perf_counter.cpp
+
+CPPSOURCES += src/modules/uORB/Subscription.cpp
+CPPSOURCES += src/modules/uORB/uORB.cpp
+CPPSOURCES += src/modules/uORB/uORBDeviceMaster.cpp
+CPPSOURCES += src/modules/uORB/uORBDeviceNode.cpp
+CPPSOURCES += src/modules/uORB/uORBMain.cpp
+CPPSOURCES += src/modules/uORB/uORBManager.cpp
+CPPSOURCES += src/modules/uORB/uORBUtils.cpp
+
+CPPSOURCES += src/modules/uORB/topics_sources/log_message.cpp
 
 #####################################
 # app configuration
 #####################################
-
 PROJ_CDEFS += BSP_COM_PRINTF
 PROJ_CDEFS += BSP_MODULE_FR
 #PROJ_CDEFS += BSP_MODULE_USB_CHERRY
